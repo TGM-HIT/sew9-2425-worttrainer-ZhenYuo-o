9f0ddf08af08794d1302c37af74649d2d907d1d1
ZhenYu <zhenn.whoop@gmail.com>
2024-09-20 11:17:25 +0200

    feat(worteintrag): Worteintrag

--- app/src/main/java/model/WortEintrag.java
+++ app/src/main/java/model/WortEintrag.java
@@ -1,5 +1,56 @@
 package model;
 
-public class WortEintrag {
+import java.io.Serializable;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.util.Objects;
 
+public class WortEintrag implements Serializable {
+    static final long serialVersionUID = 1L;
+    private String wort;
+    private String url;
+
+    public WortEintrag(String wort, String url) {
+        this.wort = validateString(wort, "Wort");
+        this.url = validateString(url, "URL");
+
+        if (wort.trim().length() < 2) {
+            throw new IllegalArgumentException("Das Wort muss mindestens zwei Zeichen lang sein und darf nicht nur aus Leerzeichen bestehen.");
+        }
+
+        if (!checkURL(url)) {
+            throw new IllegalArgumentException("Die URL ist nicht gültig!");
+        }
+    }
+
+    public boolean checkURL(String url) {
+    try {
+        new URI(url);
+        return true;
+    } catch (Exception e) {
+        return false;
+      }
+    }
+    public void setUrl(String url) {
+        if (checkURL(url)) {
+            this.url = url;
+        } else {
+            throw new IllegalArgumentException("Die URL ist nicht gültig!");
+        }
+    }
+    public String getWort() {return this.wort;}
+    public String getUrl() {return this.url;}
+
+    private String validateString(String value, String fieldName) {
+        Objects.requireNonNull(value, fieldName + " darf nicht null sein");
+        value = value.trim();
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        String text = "";
+        return text + (this.wort + "\n" + this.url);
+    }
 }
