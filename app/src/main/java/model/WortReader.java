d9f0cbe6018b806b9736b96a17e0942830026235
zhenyu <zzhan@student.tgm.ac.at>
2024-10-05 19:29:04 +0200

    feat(wortReader): alle wörter und Links werden in woerter.csv gespeichert

---
+++ app/src/main/java/model/WortReader.java
@@ -0,0 +1,37 @@
+package model;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class WortReader {
+
+    private final List<WortEintrag> WORTEINTRAEGE = new ArrayList<>();
+
+    public WortReader(String path) {
+        readQuestionsFile(path);
+    }
+
+    public void readQuestionsFile(String path) {
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] parts = line.split(",");
+                if (parts.length == 2) {
+                    String wort = parts[0].trim();
+                    String url = parts[1].trim();
+                    WortEintrag eintrag = new WortEintrag(wort, url);
+                    WORTEINTRAEGE.add(eintrag);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<WortEintrag> getWORTEINTRAEGE() {
+        return WORTEINTRAEGE;
+    }
+}
